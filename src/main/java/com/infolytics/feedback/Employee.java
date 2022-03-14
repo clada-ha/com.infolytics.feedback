8914ee6e146dad3c8d7e5ee8dab1668fa4c99710
Claudia Harder <charder@infolytics.com>
2022-03-14 18:04:57 +0100

    Employee and Entry class

---
+++ src/main/java/com/infolytics/feedback/Employee.java
@@ -0,0 +1,7 @@
+package com.infolytics.feedback;
+
+public class Employee {
+    private String firstname;
+    private String lastname;
+    private String identifier;
+}
---
+++ src/main/java/com/infolytics/feedback/Entry.java
@@ -0,0 +1,4 @@
+package com.infolytics.feedback;
+
+public class Entry {
+}
